58ad9e4d9bb66c11c266b101ed2b0cc3017d6b60
Prabhash Gupta <33936407+prabhashgupta@users.noreply.github.com>
2021-11-14 13:27:10 +0530

    Update main.ts

--- server/main.ts
+++ server/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
+import { config } from 'dotenv';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  config();
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   await app.listen(3000, async () => {
