582aae9b2970a32b4f48ea710ed5bdecc068f39d
Prabhash Gupta <33936407+prabhashgupta@users.noreply.github.com>
2021-11-14 12:48:56 +0530

    Update main.js

--- client/main.js
+++ client/main.js
@@ -10,23 +10,38 @@ function validate() {
 
 
 function loadResult() {
-    const url = `http://localhost:3000/api/movie-search?movieSearchKey=${document.searchForm.movieSearchKey.value}&movieSearchCategory=${document.searchForm.movieSearchCategory.value}`;
+    const url = `http://localhost:3000/api/medialist/movie-search?movieSearchKey=${document.searchForm.movieSearchKey.value}&movieSearchCategory=${document.searchForm.movieSearchCategory.value}`;
     fetch(url, {
         method: 'GET',
         headers: { 'Content-Type': 'application/json' }
     }).then(async response => {
-        document.getElementById("movie-grid").innerHTML = JSON.stringify(await response.json());
+        const data = await response.json();
+        
+        if (data instanceof Array && data.length) {
+            let renderHtml = `<table><tr><th>Type</th><th>Name</th><th>Year</th><th>Poster</th></tr>`
+            data.forEach(el => {
+                renderHtml += `<tr>
+                <td>${el.type}</td>
+                <td>${el.title}</td>
+                <td>${el.year}</td>
+                <td>
+                    <img src="${el.poster}" alt="${el.title}" width="100" height="120">
+                </td></tr>`;
+            })
+
+            renderHtml += `</table>`
+            
+
+            document.getElementById("movie-grid").innerHTML = renderHtml;
+
+        } else {
+            document.getElementById("movie-grid").innerHTML = "No Records Found!"
+        }
+        
         return (true);
     });
 
-    const data = `<table>
-<tr>
-    <th>Type</th>
-    <th>Name</th>
-    <th>Year</th>
-    <th>Poster</th>
-</tr>
-<tr>
+    const data = `
     <td>Movie</td>
     <td>Name</td>
     <td>2021</td>
