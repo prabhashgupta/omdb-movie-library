a79498f17ba6f8b3b23daa013ee8327ffc2052cf
Prabhash Gupta <33936407+prabhashgupta@users.noreply.github.com>
2021-11-14 12:51:16 +0530

    Update app.module.ts

--- server/app.module.ts
+++ server/app.module.ts
@@ -2,18 +2,25 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MedialistModule } from './medialist/medialist.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({}),
+    TypeOrmModule.forRoot({
+      name: 'default',
+      type: 'sqlite',
+      database: 'db',
+      logging: true,
+      synchronize: true,
+      autoLoadEntities: true
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client'),
       exclude: ['/api*'],
     }),
+    MedialistModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
